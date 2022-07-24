21f91bb1391420d18a5b4ab7e318b49e0bab5eac
Nathan Klisch <nklisch@gmail.com>
2022-07-23 20:10:55 -0600

    tests: added extensive parser tests

--- src/__tests__/openApiPath.test.ts
+++ src/__tests__/openApiPath.test.ts
@@ -1,37 +1,36 @@
-import { OpenAPIV3 } from 'openapi-types'
-//import OpenApiPath from '../middleware/openApiPath'
-it('detects the openApiPath middleware', (done)=>{
-    
-    // const p: OpenAPIV3.OperationObject = {
-    //     "description": "Returns pets based on ID",
-    //     "summary": "Find pets by ID",
-    //     "operationId": "getPetsById",
-    //     "responses": {
-    //         "200": {
-    //             "description": "pet response",
-    //             "content": {
-    //                 "*/*": {
-    //                     "schema": {
-    //                         "type": "array",
-    //                         "items": {
-    //                             "$ref": "#/components/schemas/Pet"
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         },
-    //         "default": {
-    //             "description": "error payload",
-    //             "content": {
-    //                 "text/html": {
-    //                     "schema": {
-    //                         "$ref": "#/components/schemas/ErrorModel"
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
-    // const openApiPath = new OpenApiPath({});
-    done();
-})
+import { OpenAPIV3 } from 'openapi-types';
+// import OpenApiPath from '../middleware/openApiPath'
+it('detects the openApiPath middleware', (done) => {
+  // const p: OpenAPIV3.OperationObject = {
+  //     "description": "Returns pets based on ID",
+  //     "summary": "Find pets by ID",
+  //     "operationId": "getPetsById",
+  //     "responses": {
+  //         "200": {
+  //             "description": "pet response",
+  //             "content": {
+  //                 "*/*": {
+  //                     "schema": {
+  //                         "type": "array",
+  //                         "items": {
+  //                             "$ref": "#/components/schemas/Pet"
+  //                         }
+  //                     }
+  //                 }
+  //             }
+  //         },
+  //         "default": {
+  //             "description": "error payload",
+  //             "content": {
+  //                 "text/html": {
+  //                     "schema": {
+  //                         "$ref": "#/components/schemas/ErrorModel"
+  //                     }
+  //                 }
+  //             }
+  //         }
+  //     }
+  // }
+  // const openApiPath = new OpenApiPath({});
+  done();
+});
