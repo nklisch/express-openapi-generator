87c6928f3d792f0de0bf90597fcef19dd09f8e59
Nathan Klisch <nklisch@gmail.com>
2022-08-04 17:33:23 -0600

    docs: gen-docs

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ export { default as PathMiddleware } from './express-openapi-middleware';
 export { default as OperationBuilder, OperationDefaults } from './openapi-builder/operationBuilder';
 export { default as ResponseBuilder, ResponseDefaults } from './openapi-builder/responseBuilder';
 export { default as ParameterBuilder } from './openapi-builder/parameterBuilder';
-export { default as RequestBodyBuilder } from './openapi-builder/requestBodyBuilder';
+export { default as RequestBodyBuilder, RequestBodyDefaults } from './openapi-builder/requestBodyBuilder';
 export { ComponentFieldNames, CompositeSchemaTypes, ComponentParameter, Component } from './types/index';
